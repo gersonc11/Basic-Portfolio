13b4c5f4240ccf565fccef99e19e69bd9536b825
Gerson Cruz <gcruz1776@gmail.com>
2019-02-15 23:31:40 -0600

    updated links

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,4 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.ui.accordion').accordion();
-    
- });
+    function once(fn, context) {
+        var result;
+        return function () {
+            if (fn) {
+                result = fn.apply(context || this, arguments);
+                fn = null;
+            }
+            return result;
+        };
+    }
+    $('.call').hover(once(function () {
+        $(this).append("<p>469-247-5313</p>")
+    }))
+    $('.email').hover(once(function () {
+        $(this).append("<p>gersoncruz91@gmail.com</p>")
+    }))
+    $('.page').hover(function() {
+        
+    })
+});

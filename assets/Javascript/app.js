eb5acfa05e224e5872c40f3d494d44d62ab2dba3
Gerson Cruz <gcruz1776@gmail.com>
2019-02-19 01:52:25 -0600

    added Firebase Database

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,6 +1,45 @@
 $(document).ready(function () {
+
+      // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDB8adcifVgeemcIx93qI3kk_mQWGRhmnw",
+    authDomain: "portfolio-e76ef.firebaseapp.com",
+    databaseURL: "https://portfolio-e76ef.firebaseio.com",
+    projectId: "portfolio-e76ef",
+    storageBucket: "portfolio-e76ef.appspot.com",
+    messagingSenderId: "895600960467"
+  };
+  firebase.initializeApp(config);
+  
+  var dataBase = firebase.database();
+
+  $("#send").on("click",  () => {
+    event.preventDefault();
+    // Grabs user input
+    var name = $("#name").val().trim();
+    var email = $("#email").val().trim();
+    var message = $("#message").val().trim();
+
+
+    dataBase.ref().push({
+        name: name,
+        email: email,
+        message: message
+    })
+
+
+
+
+    // Clears all of the text-boxes
+    $("#name").val("");
+    $("#email").val("");
+    $("#message").val("");
+});
+
+
+
     $('.ui.accordion').accordion();
-    
+
     let once = (fn, context) => {
         var result;
         return function () {
@@ -19,7 +58,8 @@ $(document).ready(function () {
     $('.email').hover(once(function () {
         $(this).append("<p>gersoncruz91@gmail.com</p>")
     }))
-    $('.page').hover(function() {
 
-    })
+ 
+
+
 });
